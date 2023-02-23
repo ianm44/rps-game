740599497ccb23c2b3eee353e3734df55f650e17
ianm44 <ianmcc1994@gmail.com>
2023-02-23 12:53:49 -0800

    Complete Computer Choice
    Finished building function to create random computer choice in rps game.
    
    Created prompt to ask user for a choice and convert choice to specific format(lowercase with uppercase first letter)

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
-rpsPossibilities = ['Rock', 'Paper', 'Scissors']
+const rpsPossibilities = ['Rock', 'Paper', 'Scissors']
 
-function getComputerChoice() {
-    Math.floor(Math.random() * rpsPossibilities.length);
+function getComputerChoice(rpsPossibilities) {
+   const arrayRandomizer = Math.floor(Math.random() * rpsPossibilities.length);
+   return rpsPossibilities[arrayRandomizer];
 }
 
-const computerSelection = getComputerChoice();
+const computerSelection = getComputerChoice(rpsPossibilities);
+console.log(computerSelection);
 
-console.log(computerSelection)
-
+const playerSelection = prompt("Choose your weapon!");
